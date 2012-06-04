cab48a39d1c41afcd877a107ca51dca27cbb3fde
Brendan Forster <me@brendanforster.com>
2012-06-04 18:37:18 +0200

    a test!

---
+++ src/CloakedHipster.Tests/StyleParserTests.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace CloakedHipster.Tests
+{
+    public class StyleParserTests
+    {
+        [Fact]
+        public void Parse_WithEmptyValue_ReturnsEmptyResourcesSet()
+        {
+            var parser = new StyleParser();
+            var result = parser.Parse("");
+            Assert.Empty(result);
+        }
+    }
+
+    public class StyleParser
+    {
+        public IEnumerable<Resource> Parse(string css)
+        {
+            return Enumerable.Empty<Resource>();
+        }
+    }
+
+    public class Resource
+    {
+    }
+}
